a2b6d3bb3c20afec2d0e542609eb4131be3686bb
Twisted7ech <bradcederberg@gmail.com>
2022-06-08 14:44:51 -0600

    added logo, rearranged text

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,31 @@
-const deleteText = document.querySelectorAll('.fa-trash')
-const thumbText = document.querySelectorAll('.fa-thumbs-up')
+// const deleteText = document.querySelectorAll('.fa-trash')
+const thumbText = document.querySelectorAll('.fa-arrow-circle-up')
 
-Array.from(deleteText).forEach((element)=>{
-    element.addEventListener('click', deleteRapper)
-})
+// Array.from(deleteText).forEach((element)=>{
+//     element.addEventListener('click', deleteRapper)
+// })
 
 Array.from(thumbText).forEach((element)=>{
     element.addEventListener('click', addLike)
 })
 
-async function deleteRapper(){
+
+
+async function addLike(){
     const sName = this.parentNode.childNodes[1].innerText
     const bName = this.parentNode.childNodes[3].innerText
+    const tLikes = Number(this.parentNode.childNodes[5].innerText)
     try{
-        const response = await fetch('deleteRapper', {
-            method: 'delete',
+        const response = await fetch('addOneRank', {
+            method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-              'stageNameS': sName,
-              'birthNameS': bName
+              'character': sName,
+              'phrase': bName,
+              'season': season,
+              'episode': episode,
+              'time': time
+              'rank': tLikes
             })
           })
         const data = await response.json()
@@ -30,18 +37,16 @@ async function deleteRapper(){
     }
 }
 
-async function addLike(){
+async function deleteRapper(){
     const sName = this.parentNode.childNodes[1].innerText
     const bName = this.parentNode.childNodes[3].innerText
-    const tLikes = Number(this.parentNode.childNodes[5].innerText)
     try{
-        const response = await fetch('addOneLike', {
-            method: 'put',
+        const response = await fetch('deleteRapper', {
+            method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
               'stageNameS': sName,
-              'birthNameS': bName,
-              'likesS': tLikes
+              'birthNameS': bName
             })
           })
         const data = await response.json()
